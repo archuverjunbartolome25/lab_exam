8965a36ccc433bff93ef1338170b560ae3923053
ajbuttowski25 <archuverjunbartolome25@gmail.com>
2026-04-07 22:56:58 +0800

    aaa
    sss

---
+++ resources/js/components/TextLink.vue
@@ -0,0 +1,11 @@
+<template>
+    <a
+        :href="href"
+        :class="[
+            'inline-flex items-center gap-2 text-sm font-medium transition-colors hover:text-primary underline-offset-4 hover:underline',
+            active && 'text-primary'
+        ]"
+    >
+        <slot />
+    </a>
+</template>
---
+++ resources/js/layouts/settings/Layout.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <main class="py-6">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <slot />
+            </div>
+        </main>
+    </div>
+</template>
